9e0797448fcf6e29d02c1fa8f74574ac29f09bd5
Battig Felix <battigfelix@Felixs-MacBook-Pro.local>
2018-05-27 08:14:02 -0400

    Add state to a component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-    {
-        'id': 'ryan',
-        'name': 'Ryan Florence',
-        'email': 'ryan@yahoo.com',
-        'avatarURL': 'http://localhost:5001/ryan.jpg'
-    },
-    {
-        'id': 'michael',
-        'name': 'Michael Foster',
-        'email': 'michael@yahoo.com',
-        'avatarURL': 'http://localhost:5001/michael.jpg'
-    },
-    {
-        'id': 'tyler',
-        'name': 'Tyler MacDonals',
-        'email': 'tyler@yahoo.com',
-        'avatarURL': 'http://localhost:5001/tyler.jpg'
-    },
-] 
+
 
 class App extends Component {
+    state = {
+        contacts : [
+            {
+                'id': 'ryan',
+                'name': 'Ryan Florence',
+                'email': 'ryan@yahoo.com',
+                'avatarURL': 'http://localhost:5001/ryan.jpg'
+            },
+            {
+                'id': 'michael',
+                'name': 'Michael Foster',
+                'email': 'michael@yahoo.com',
+                'avatarURL': 'http://localhost:5001/michael.jpg'
+            },
+            {
+                'id': 'tyler',
+                'name': 'Tyler MacDonals',
+                'email': 'tyler@yahoo.com',
+                'avatarURL': 'http://localhost:5001/tyler.jpg'
+            },
+        ] 
+    }
     render() {
         return (
             <div>
-                <ListContacts contacts={contacts} />
+                <ListContacts contacts={this.state.contacts} />
             </div>
         )
     }
