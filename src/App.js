d24093a74ef49f4b403d88d31bc110534e7ade02
Battig Felix <battigfelix@Felixs-MacBook-Pro.local>
2018-05-27 07:31:10 -0400

    ListContacts and Styling

--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
-import * as ContactsAPI from './utils/ContactsAPI'
+import ListContacts from './ListContacts';
+
+const contacts = [
+    {
+        'id': 'ryan',
+        'name': 'Ryan Florence',
+        'email': 'ryan@yahoo.com',
+        'avatarURL': 'http://localhost:5001/ryan.jpg'
+    },
+    {
+        'id': 'michael',
+        'name': 'Michael Foster',
+        'email': 'michael@yahoo.com',
+        'avatarURL': 'http://localhost:5001/michael.jpg'
+    },
+    {
+        'id': 'tyler',
+        'name': 'Tyler MacDonals',
+        'email': 'tyler@yahoo.com',
+        'avatarURL': 'http://localhost:5001/tyler.jpg'
+    },
+] 
 
 class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <ListContacts contacts={contacts} />
+            </div>
+        )
+    }
 }
 
 export default App;
